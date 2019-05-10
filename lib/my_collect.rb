4182c881448591440c704596a1a65bb8d5ddbd91
Elma Rahman <elmarahman635@gmail.com>
2019-05-10 22:48:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -14,10 +14,8 @@
    name_collection = []
     while i < array.length
     name_collection.push(yield(array[i]))
-      
+
         i += 1
       end
       name_collection
 end
-
-my_collect(list) {|i| i.split(" ").first}
