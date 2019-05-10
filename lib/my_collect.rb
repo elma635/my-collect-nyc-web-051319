0bfbf22f4fb4cc39ccd28ad96f10432ed7b0182d
Elma Rahman <elmarahman635@gmail.com>
2019-05-10 22:34:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-def my_collect(array)
-i = 0
-
-yield(array[i])
-name.split(" ").first
-
-end
+# def my_collect(array)
+# i = 0
+#
+# yield(array[i])
+# name.split(" ").first
+#
+# end
 
 
  def my_collect(array)
@@ -14,8 +14,7 @@ end
    name_collection = []
     while i < array.length
       name_collection.push
-      yield
-      (array[i])
+      yield(array[i])
         i += 1
       end
       name_collection
